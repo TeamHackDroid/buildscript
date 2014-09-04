d00e2e8cdb2c8704acb7b658e5c4194bceaaf215
hak86 <hakamelassouad@gmail.com>
2014-09-04 13:02:02 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@ export USE_CCACHE=1
 continue=0
 
 while [ $continue -eq "0" ]; do
-	echo "Select device: 
+	echo "Welcome to Team Hack Droid, do you have problems to start compilation? just choose your phone and will
+	build everything automatically:
+	
 	ancora
 	ancora_tmo
 	s2ve
 	s2vep"
-	read device
+	read device 
 	if [ "$device" = "ancora" ] || [ "$device" = "ancora_tmo" ] || [ "$device" = "s2ve" ] || [ "$device" = "s2vep" ] ; then 
 		continue=1
 	else
