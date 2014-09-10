407cae30cc3b7ef83e5fe947274b34fd56cd49e8
doadin <tbkizle@gmail.com>
2014-09-10 17:30:07 -0400

    Improve Kernel Script Compile Time
    *Add variable with number of CPU cores
    *Compile the kernel in parallel based on the computers CPU cores

--- Kernel Tools/Kernel Compile.sh
+++ Kernel Tools/Kernel Compile.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#Get number of proccessors
+NPROC=$(nproc)
+
 #Clean Temp Dirs
 rm -rf  ~/kernel/KKernel/temp/ancora_tmo_defconfig/*
 rm -rf  ~/kernel/KKernel/temp/ancora_tmo_oc_exuv_hm_defconfig/*
@@ -67,7 +70,7 @@ export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/kernel/toolchains/linaro_toolchains_2014-2014.08/arm-cortex_a8-linux-gnueabi-linaro_4.9.2-2014.08/bin/arm-cortex_a8-linux-gnueabi-
 make ancora_tmo_defconfig
-make
+make -j$NPROC
 cp  ~/kernel/KKernel/build/arch/arm/boot/zImage ~/kernel/KKernel/temp/ancora_tmo_defconfig/zImage
 cp  ~/kernel/KKernel/build/fs/cifs/cifs.ko ~/kernel/KKernel/temp/ancora_tmo_defconfig/system/modules/cifs.ko
 cp  ~/kernel/KKernel/build/drivers/net/wireless/bcmdhd/dhd.ko ~/kernel/KKernel/temp/ancora_tmo_defconfig/system/modules/dhd.ko
@@ -87,7 +90,7 @@ export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/kernel/toolchains/linaro_toolchains_2014-2014.08/arm-cortex_a8-linux-gnueabi-linaro_4.9.2-2014.08/bin/arm-cortex_a8-linux-gnueabi-
 make ancora_tmo_oc_exuv_hm_defconfig
-make
+make -j$NPROC
 cp  ~/kernel/KKernel/build/arch/arm/boot/zImage ~/kernel/KKernel/temp/ancora_tmo_oc_exuv_hm_defconfig/zImage
 cp  ~/kernel/KKernel/build/fs/cifs/cifs.ko ~/kernel/KKernel/temp/ancora_tmo_oc_exuv_hm_defconfig/system/modules/cifs.ko
 cp  ~/kernel/KKernel/build/drivers/net/wireless/bcmdhd/dhd.ko ~/kernel/KKernel/temp/ancora_tmo_oc_exuv_hm_defconfig/system/modules/dhd.ko
@@ -107,7 +110,7 @@ export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/kernel/toolchains/linaro_toolchains_2014-2014.08/arm-cortex_a8-linux-gnueabi-linaro_4.9.2-2014.08/bin/arm-cortex_a8-linux-gnueabi-
 make ancora_tmo_oc_exuv_sm_defconfig
-make
+make -j$NPROC
 cp  ~/kernel/KKernel/build/arch/arm/boot/zImage ~/kernel/KKernel/temp/ancora_tmo_oc_exuv_sm_defconfig/zImage
 cp  ~/kernel/KKernel/build/fs/cifs/cifs.ko ~/kernel/KKernel/temp/ancora_tmo_oc_exuv_sm_defconfig/system/modules/cifs.ko
 cp  ~/kernel/KKernel/build/drivers/net/wireless/bcmdhd/dhd.ko ~/kernel/KKernel/temp/ancora_tmo_oc_exuv_sm_defconfig/system/modules/dhd.ko
@@ -127,7 +130,7 @@ export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/kernel/toolchains/linaro_toolchains_2014-2014.08/arm-cortex_a8-linux-gnueabi-linaro_4.9.2-2014.08/bin/arm-cortex_a8-linux-gnueabi-
 make ancora_tmo_oc_exuv_vhm_defconfig
-make
+make -j$NPROC
 cp  ~/kernel/KKernel/build/arch/arm/boot/zImage ~/kernel/KKernel/temp/ancora_tmo_oc_exuv_vhm_defconfig/zImage
 cp  ~/kernel/KKernel/build/fs/cifs/cifs.ko ~/kernel/KKernel/temp/ancora_tmo_oc_exuv_vhm_defconfig/system/modules/cifs.ko
 cp  ~/kernel/KKernel/build/drivers/net/wireless/bcmdhd/dhd.ko ~/kernel/KKernel/temp/ancora_tmo_oc_exuv_vhm_defconfig/system/modules/dhd.ko
@@ -147,7 +150,7 @@ zip -d KKernel-`date +%Y%m%d_%H%M`-oc-exuv-vhm.zip "zImage"
 #export SUBARCH=arm
 #export CROSS_COMPILE=/home/doadin/Desktop/kernel/toolchains/linaro_toolchains_2014-2014.03/arm-cortex_a8-linux-gnueabi-#linaro_4.8.3-2014.03/bin/arm-gnueabi-
 #make ancora_tmo_oc_exuv_xhm_defconfig
-#make
+#make -j$NPROC
 #cp  ~/kernel/KKernel/build/arch/arm/boot/zImage ~/kernel/KKernel/temp/#ancora_tmo_oc_exuv_xhm_defconfig/zImage
 #cp  ~/kernel/KKernel/build/fs/cifs/cifs.ko ~/kernel/KKernel/temp/#ancora_tmo_oc_exuv_xhm_defconfig/cifs.ko
 #cp  ~/kernel/KKernel/build/drivers/net/wireless/bcmdhd/dhd.ko ~/kernel/KKernel/temp/#ancora_tmo_oc_exuv_xhm_defconfig/system/modules/dhd.ko
@@ -167,7 +170,7 @@ export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/kernel/toolchains/linaro_toolchains_2014-2014.08/arm-cortex_a8-linux-gnueabi-linaro_4.9.2-2014.08/bin/arm-cortex_a8-linux-gnueabi-
 make ancora_tmo_oc_uv_hm_defconfig
-make
+make -j$NPROC
 cp  ~/kernel/KKernel/build/arch/arm/boot/zImage ~/kernel/KKernel/temp/ancora_tmo_oc_uv_hm_defconfig/zImage
 cp  ~/kernel/KKernel/build/fs/cifs/cifs.ko ~/kernel/KKernel/temp/ancora_tmo_oc_uv_hm_defconfig/system/modules/cifs.ko
 cp  ~/kernel/KKernel/build/drivers/net/wireless/bcmdhd/dhd.ko ~/kernel/KKernel/temp/ancora_tmo_oc_uv_hm_defconfig/system/modules/dhd.ko
@@ -187,7 +190,7 @@ export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/kernel/toolchains/linaro_toolchains_2014-2014.08/arm-cortex_a8-linux-gnueabi-linaro_4.9.2-2014.08/bin/arm-cortex_a8-linux-gnueabi-
 make ancora_tmo_oc_uv_sm_defconfig
-make
+make -j$NPROC
 cp  ~/kernel/KKernel/build/arch/arm/boot/zImage ~/kernel/KKernel/temp/ancora_tmo_oc_uv_sm_defconfig/zImage
 cp  ~/kernel/KKernel/build/fs/cifs/cifs.ko ~/kernel/KKernel/temp/ancora_tmo_oc_uv_sm_defconfig/system/modules/cifs.ko
 cp  ~/kernel/KKernel/build/drivers/net/wireless/bcmdhd/dhd.ko ~/kernel/KKernel/temp/ancora_tmo_oc_uv_sm_defconfig/system/modules/dhd.ko
@@ -207,7 +210,7 @@ export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/kernel/toolchains/linaro_toolchains_2014-2014.08/arm-cortex_a8-linux-gnueabi-linaro_4.9.2-2014.08/bin/arm-cortex_a8-linux-gnueabi-
 make ancora_tmo_oc_uv_vhm_defconfig
-make
+make -j$NPROC
 cp  ~/kernel/KKernel/build/arch/arm/boot/zImage ~/kernel/KKernel/temp/ancora_tmo_oc_uv_vhm_defconfig/zImage
 cp  ~/kernel/KKernel/build/fs/cifs/cifs.ko ~/kernel/KKernel/temp/ancora_tmo_oc_uv_vhm_defconfig/system/modules/cifs.ko
 cp  ~/kernel/KKernel/build/drivers/net/wireless/bcmdhd/dhd.ko ~/kernel/KKernel/temp/ancora_tmo_oc_uv_vhm_defconfig/system/modules/dhd.ko
@@ -227,7 +230,7 @@ zip -d KKernel-`date +%Y%m%d_%H%M`-oc-uv-vhm.zip "zImage"
 #export SUBARCH=arm
 #export CROSS_COMPILE=/home/doadin/Desktop/kernel/toolchains/linaro_toolchains_2014-2014.03/arm-cortex_a8-linux-gnueabi-#linaro_4.8.3-2014.03/bin/arm-gnueabi-
 #make ancora_tmo_oc_uv_xhm_defconfig
-#make
+#make -j$NPROC
 #cp  ~/kernel/KKernel/build/arch/arm/boot/zImage ~/kernel/KKernel/temp/#ancora_tmo_oc_uv_xhm_defconfig/zImage
 #cp  ~/kernel/KKernel/build/fs/cifs/cifs.ko ~/kernel/KKernel/temp/#ancora_tmo_oc_uv_xhm_defconfig/cifs.ko
 #cp  ~/kernel/KKernel/build/drivers/net/wireless/bcmdhd/dhd.ko ~/kernel/KKernel/temp/#ancora_tmo_oc_uv_xhm_defconfig/system/modules/dhd.ko
@@ -250,7 +253,7 @@ export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/kernel/toolchains/linaro_toolchains_2014-2014.08/arm-cortex_a8-linux-gnueabi-linaro_4.9.2-2014.08/bin/arm-cortex_a8-linux-gnueabi-
 make ancora_tmo_defconfig
-make
+make -j$NPROC
 cp  ~/kernel/ADC/build/arch/arm/boot/zImage ~/kernel/ADC/temp/ancora_tmo_defconfig/zImage
 cp  ~/kernel/ADC/build/fs/cifs/cifs.ko ~/kernel/ADC/temp/ancora_tmo_defconfig/system/modules/cifs.ko
 cp  ~/kernel/ADC/build/drivers/net/wireless/bcmdhd/dhd.ko ~/kernel/ADC/temp/ancora_tmo_defconfig/system/modules/dhd.ko
